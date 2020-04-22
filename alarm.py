35284b5ec0be7773514fa37f85ab436eacfbce40
Drew <52759042+Drew-Alleman@users.noreply.github.com>
2020-04-22 16:52:14 -0700

    Update alarm.py

--- alarm.py
+++ alarm.py
@@ -1,42 +1,40 @@
-import RPi.GPIO as GPIO
-import logging
 import time
- 
-#set GPIO Pins
-GPIO_TRIGGER = 26
-pinEcho = 19
-GPIO_ECHO = 13
+from time import sleep
+from datetime import datetime
+import RPi.GPIO as GPIO
+from Adafruit_CharLCD import Adafruit_CharLCD
 
-logging.basicConfig(format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S',level=logging.DEBUG)
+# instantiate lcd and specify pins
+lcd = Adafruit_CharLCD(rs=26, en=19,
+                       d4=13, d5=6, d6=5, d7=11,
+                       cols=16, lines=2) #LCD pins change to desire
+lcd.clear() # clear LCD screen
+old_time = "" # Old time value
+GPIO.setmode(GPIO.BCM) # Set Board mode 
+GPIO_BUZZER = 18 # Buzzer Pin
+GPIO.setup(GPIO_BUZZER, GPIO.OUT) # Set buzzer pin to output
 
-#set GPIO direction (IN / OUT)
-GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
-GPIO.setup(GPIO_ECHO, GPIO.IN)
+now = datetime.now()
+hour = input("Enter hour: ") # Asks user for Hour
+min = input("Enter Minute: ") # asks user for minute
+time_day = input("AM or PM: ")
+alarm = now.replace(hour=int(hour), minute=int(min), second=0, microsecond=0) # Puts input into time value to compare later
+alarm_print = alarm.strftime("%H:%M:%S "+time_day) # Pretty Print
+alarm_max = now.replace(hour=int(hour), minute=int(min)+30, second=0, microsecond=0)
+
+while True: # Loop forever
+	now = datetime.now() # Get Current time
+	current_time = now.strftime("%H:%M:%S %p") # Print that time and alarm time \n = New Line
+	if old_time != current_time: # If time has changed by one second. If this bit was removed it would print the time every few nano-seconds
+		lcd.clear() # clear screen
+		lcd.message(current_time+"\n"+alarm_print) # Print time to LCD screen
+		if now >= alarm & time_day in current_time and now <= alarm_max: # if enough time has passed and the time > than the alarm	and its less than 30 minutes ahead		
+			lcd.clear() # clear screen
+			lcd.message("WAKE UP") # Display message wake up
+			GPIO.output(GPIO_BUZZER, True) # Turn on the buzzer
+			time.sleep(1) # Sleep for __ seconds and leave the buzzer on
+			GPIO.output(GPIO_BUZZER,False) # Turn Buzzer back off
+			lcd.clear() # Clear screen again
+			exit(0)	 # Exit
+		old_time = current_time
  
-def distance():
-    GPIO.output(GPIO_TRIGGER, True)
-    time.sleep(0.00001)
-    GPIO.output(GPIO_TRIGGER, False)
- 
-    StartTime = time.time()
-    StopTime = time.time()
-    while GPIO.input(GPIO_ECHO) == 0:
-        StartTime = time.time()
-    while GPIO.input(GPIO_ECHO) == 1:
-        StopTime = time.time()
-    TimeElapsed = StopTime - StartTime
-    distance = (TimeElapsed * 34300) / 2
- 
-    return distance
- 
-if __name__ == '__main__':
-    try:
-        while True:
-            dist = distance()
-            print ("Measured Distance = %.1f cm" % dist)
-            time.sleep(1)
- 
-        # Reset by pressing CTRL + C
-    except KeyboardInterrupt:
-        print("Measurement stopped by User")
-        GPIO.cleanup()
