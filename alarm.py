9b3ee0fc24edb60676ad0334542cab86fb4694f7
Drew <52759042+Drew-Alleman@users.noreply.github.com>
2020-04-22 17:49:41 -0700

    Update alarm.py

--- alarm.py
+++ alarm.py
@@ -27,13 +27,14 @@ while True: # Loop forever
 	if old_time != current_time: # If time has changed by one second. If this bit was removed it would print the time every few nano-seconds
 		lcd.clear() # clear screen
 		lcd.message(current_time+"\n"+alarm_print) # Print time to LCD screen
-		if now >= alarm &  time_day in current_time: # if enough time has passed and the time > than the alarm	and its less than 30 minutes ahead		
-			lcd.clear() # clear screen
-			lcd.message("WAKE UP") # Display message wake up
-			GPIO.output(GPIO_BUZZER, True) # Turn on the buzzer
-			time.sleep(1) # Sleep for __ seconds and leave the buzzer on
-			GPIO.output(GPIO_BUZZER,False) # Turn Buzzer back off
-			lcd.clear() # Clear screen again
-			exit(0)	 # Exit
+		if now >= alarm 
+			if time_day in current_time: # if enough time has passed and the time > than the alarm	and its less than 30 minutes ahead		
+				lcd.clear() # clear screen
+				lcd.message("WAKE UP") # Display message wake up
+				GPIO.output(GPIO_BUZZER, True) # Turn on the buzzer
+				time.sleep(1) # Sleep for __ seconds and leave the buzzer on
+				GPIO.output(GPIO_BUZZER,False) # Turn Buzzer back off
+				lcd.clear() # Clear screen again
+				exit(0)	 # Exit
 		old_time = current_time
  
